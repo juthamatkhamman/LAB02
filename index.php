69ba3c7e9d7fbceadf6516bcc8b65752e4a01070
Chanothit <chanothit37372@gmail.com>
2023-01-20 00:19:03 +0700

    1

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
     <h1>Personal Information</h1>
     <form action="show.php">
-        what is your name :<input type="text" name="name"><br>
+        what is your :<input type="text" name="name"><br>
         what is your age :<input type="text" name="age"><br>
         what is your gender (male/female):<input type="text" name="gender"><br>
         what is your status (single/married):<input type="text" name="status"><br>
